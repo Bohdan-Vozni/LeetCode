0a0c20a5d526808524015129e09ac10a347ca045
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-26 21:18:44 +0200

    Solution Search Insert Position in arrray

---
+++ Search_Insert_Position/Search_Insert_Position/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Search_Insert_Position
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int size = nums.Length;
+
+
+            int right = size - 1;
+            int left = 0;
+
+            while (right >= left)
+            {
+                int middle = left + (right - left) / 2;
+
+                if (nums[middle] == target) return middle;
+
+                if (nums[middle] < target)
+                {
+                    left = middle + 1;
+
+                }
+
+                if (nums[middle] > target)
+                {
+                    right = middle - 1;                    
+                }
+
+            }
+
+            return right + 1;
+        }
+    }
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+
+            Console.WriteLine( sol.SearchInsert([2,4,6], 7) );
+        }
+    }
+}
