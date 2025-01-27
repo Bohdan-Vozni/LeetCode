9f771518af86aa47f99417350ff0d1df2ca4741c
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-27 15:56:59 +0200

    Task 3. Solution Longest Substring Without Repeating Characters

---
+++ Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Other_Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    internal class Other_Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if(string.IsNullOrEmpty(s)) return 0;
+
+            HashSet<char> unicqueChars = new HashSet<char>();
+            int maxLength = 0;
+            int left = 0;
+
+            for(int right = 0; right < s.Length; right++)
+            {
+                while (unicqueChars.Contains(s[right]))
+                {
+                    unicqueChars.Remove(s[left]);
+                    left++;
+                }
+
+                unicqueChars.Add(s[right]);
+                maxLength = Math.Max(maxLength, right - left +1);
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int LengthOfLongestSubstring(string s)
+            {
+                string previous = "";
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    string current = "";
+
+                    for (int j = i; j < s.Length; j++)
+                    {
+                        if (!current.Contains(s[j])) current += s[j];
+                        else break;
+                    }
+                    if (previous.Length < current.Length) previous = current;
+                }
+                return previous.Length;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            var othersol = new Other_Solution();
+
+            string str = "aabcdddddddjjjjjjjja";
+
+            int count = sol.LengthOfLongestSubstring(str);
+            int othcount = othersol.LengthOfLongestSubstring(str);
+
+            Console.WriteLine(count);
+            Console.WriteLine(othcount);
+        }
+    }
+}
