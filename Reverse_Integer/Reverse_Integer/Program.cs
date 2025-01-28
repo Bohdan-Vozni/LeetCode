776833c0b6dea57144acc152682c716017c477a6
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-28 14:51:07 +0200

    Task 7. Solution Reverse Integer number

---
+++ Reverse_Integer/Reverse_Integer/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Reverse_Integer
+{
+    public class Solution
+    {
+        public int Reverse1 (int x)
+        {
+            string revers = new string(x.ToString().Where(x => x != '-').Reverse().ToArray() );
+
+            double result = x < 0 ? double.Parse(revers) * -1 : double.Parse(revers);
+
+           
+             
+            return (int)result;
+        }
+
+        public int Reverse (int x)
+        {
+            int result = 0;
+
+            while (x != 0)
+            {
+                int digit = x % 10;
+                x /= 10;
+
+                if (result > (int.MaxValue / 10) || (result == int.MaxValue / 10 && digit > 7))
+                    return 0;
+                if (result < (int.MinValue / 10) || (result == int.MinValue / 10 && digit < -8))
+                    return 0;
+
+                result = result * 10 + digit;
+            }
+
+            return result;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var sol = new Solution();
+            int result = sol.Reverse(1534236469);
+
+            Console.WriteLine(result);
+        }
+    }
+}
