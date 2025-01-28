4c92c9984d9cb6507b957e1ae32ed8beafb57def
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-28 17:57:24 +0200

    Task 118. Solution Pascals Triangle of number

---
+++ Pascals_Triangle/Pascals_Triangle/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Pascals_Triangle
+{
+    public class Solution
+    { 
+        public IList<IList<int>> Generate (int numRows)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                if (i == 0)
+                {
+                    list.Add(new List<int> { 1 });
+                    continue;
+                }
+
+                List<int> listCurrent = new List<int>();
+                IList<int> listPast = list[i -1];
+
+                listCurrent.Add(1);
+                
+                for (int j = 0; j < listPast.Count -1; j++)
+                {
+                    listCurrent.Add(listPast[j] + listPast[j+1]);
+                }
+
+                listCurrent.Add(1);
+
+                list.Add(listCurrent);
+            }
+            return list;   
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            var result = sol.Generate(15);
+
+            foreach (var item in result)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.Write(item2.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
