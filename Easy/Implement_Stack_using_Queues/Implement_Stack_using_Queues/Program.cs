6787d6dfd91284a96b4cc1ed0910d6b0f03ee697
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-29 20:08:03 +0200

    Task 225. Easy Solution Implement Stack using Queues

---
+++ Easy/Implement_Stack_using_Queues/Implement_Stack_using_Queues/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Implement_Stack_using_Queues
+{
+    public class MyStack
+    {
+
+        public MyStack()
+        {
+
+        }
+        private Queue<int> queue = new Queue<int>();
+
+
+        public void Push(int x)
+        {
+
+            queue.Enqueue(x);
+
+            if ( queue.Count() < 2 ) return;
+            
+            for ( int i = 0; i < queue.Count() - 1 ; i++ )
+            {
+                int element = queue.Dequeue();
+                queue.Enqueue(element);
+
+            }         
+
+        }
+
+        public int Pop()
+        {
+            return queue.Dequeue();
+        }
+
+        public int Top()
+        {
+            return queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            if (queue.Count() == 0) return true;
+            else return false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack obj = new MyStack();
+            
+            obj.Push(1);
+            obj.Push(2);
+            obj.Push(3);
+            obj.Push(4);
+        }
+    }
+}
